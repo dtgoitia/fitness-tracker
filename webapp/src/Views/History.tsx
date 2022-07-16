d67f86f952cc3332c97730dd4c7b6d82a1ed8bbd
David Torralba Goitia <david.torralba.goitia@gmail.com>
2022-07-16 23:20:42 +0100

    Replace CSS-grid for CSS-flex

--- webapp/src/Views/History.tsx
+++ webapp/src/Views/History.tsx
@@ -9,30 +9,54 @@ function formatDate(date: Date): string {
   return `${day} ${timeNoSeconds}`;
 }
 
-const Grid = styled.div`
-  display: grid;
-  grid-template-columns: 7.5rem auto 4rem 4rem 7rem;
-`;
 const Col1 = styled.div`
-  grid-column-start: 1 / span 1;
-  justify-self: left;
+  order: 1;
+  flex-basis: 7.5rem;
+  flex-shrink: 0;
 `;
 const Col2 = styled.div`
-  grid-column-start: 2 / span 1;
-  justify-self: left;
+  order: 2;
+  flex-grow: 1;
+  flex-shrink: 0;
 `;
 const Col3 = styled.div`
-  grid-column-start: 3 / span 1;
-  justify-self: center;
+  order: 3;
+  flex-basis: 4rem;
+  flex-shrink: 0;
 `;
 const Col4 = styled.div`
-  grid-column-start: 4 / span 1;
-  justify-self: center;
+  order: 4;
+  flex-basis: 4rem;
+  flex-shrink: 0;
 `;
 const Col5 = styled.div`
-  grid-column-start: 5 / span 1;
-  justify-self: left;
+  order: 5;
+  flex-basis: 7rem;
+  flex-shrink: 0;
 `;
+
+const RowContainer = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: stretch;
+`;
+
+interface RowProps {
+  activity: Activity;
+  completedActivity: CompletedActivity;
+}
+function Row({ activity, completedActivity }: RowProps) {
+  const date = formatDate(completedActivity.date);
+  return (
+    <RowContainer>
+      <Col1>{date}</Col1>
+      <Col2>{activity.name}</Col2>
+      <Col3>{completedActivity.intensity}</Col3>
+      <Col4>{completedActivity.duration}</Col4>
+      <Col5>{completedActivity.notes}</Col5>
+    </RowContainer>
+  );
+}
 
 interface HistoryViewProps {
   activities: Activity[];
@@ -41,21 +65,17 @@ interface HistoryViewProps {
 function HistoryView({ history, activities }: HistoryViewProps) {
   const activityIndex = indexActivities(activities);
 
-  const gridItems: JSX.Element[] = [];
-  history.forEach((completedActivity, i) => {
-    const activity = activityIndex.get(completedActivity.id) as Activity;
-    const date = formatDate(completedActivity.date);
-    gridItems.push(<Col1 key={`${i}-1`}>{date}</Col1>);
-    gridItems.push(<Col2 key={`${i}-2`}>{activity.name}</Col2>);
-    gridItems.push(<Col3 key={`${i}-3`}>{completedActivity.intensity}</Col3>);
-    gridItems.push(<Col4 key={`${i}-4`}>{completedActivity.duration}</Col4>);
-    gridItems.push(<Col5 key={`${i}-5`}>{completedActivity.notes}</Col5>);
-  });
-
   return (
     <div>
       <h1>HistoryView</h1>
-      <Grid>{gridItems}</Grid>
+      <div>
+        {history.map((completedActivity, i) => {
+          const activity = activityIndex.get(completedActivity.id) as Activity;
+          return (
+            <Row activity={activity} completedActivity={completedActivity} />
+          );
+        })}
+      </div>
     </div>
   );
 }
