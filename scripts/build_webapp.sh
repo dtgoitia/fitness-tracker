8091a7fe3b4e082dac89848578b7993bd08ea4b5
David Torralba Goitia <david.torralba.goitia@gmail.com>
2022-07-19 15:23:59 +0100

    DevEx: use container to run pre-push build

--- scripts/build_webapp.sh
+++ scripts/build_webapp.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 
+if [[ -z "${WEBAPP_NAME}" ]]; then
+    echo "Please set WEBAPP_NAME environment variable"
+    exit 1
+fi
+
 webapp_dir="webapp"
 echo "Changing directory to '${webapp_dir}' directory..."
 cd $webapp_dir || exit 1
 
 echo "Kicking off CI-like webapp build..."
-CI="true" npm run build
+docker-compose run \
+    --env "CI=true" \
+    --rm "${WEBAPP_NAME}" \
+    npm run build
 
 last_exit_code=$?
 
