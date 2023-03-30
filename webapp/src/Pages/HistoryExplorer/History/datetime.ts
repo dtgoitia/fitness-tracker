5ad6555f188d601ca5e35b22fa09375dad001cdd
David Torralba Goitia <david.torralba.goitia@gmail.com>
2023-03-30 19:18:44 +0100

    feat: show locale time in activity history

--- webapp/src/Pages/HistoryExplorer/History/datetime.ts
+++ webapp/src/Pages/HistoryExplorer/History/datetime.ts
@@ -5,10 +5,12 @@
 //   const timeNoSeconds = time.slice(0, 5);
 //   return `${day} ${timeNoSeconds}`;
 // }
+
+/**
+ * Return the time (not date) as HH:MM format, in the locale.
+ */
 export function formatTime(date: Date): string {
-  const isoUtc = date.toISOString();
-  const noMilliseconds = isoUtc.split(".")[0];
-  const [, time] = noMilliseconds.split("T");
-  const timeNoSeconds = time.slice(0, 5);
-  return `${timeNoSeconds}`;
+  const time = date.toLocaleTimeString(); // "00:00:00" (HH:MM:SS)
+  const hhmm = time.slice(0, 5);
+  return hhmm;
 }
