d7a5065d9c843233a7929be5a73ac6e73b7147bc
David Torralba Goitia <david.torralba.goitia@gmail.com>
2023-04-14 08:46:45 +0100

    feat: format timedelta

---
+++ webapp/src/domain/datetimeUtils.test.ts
@@ -0,0 +1,49 @@
+import { formatTimedelta } from "./datetimeUtils";
+
+fdescribe("format timedelta", () => {
+  it(`s`, () => {
+    expect(formatTimedelta(1)).toEqual("1s");
+  });
+
+  it(`ss`, () => {
+    expect(formatTimedelta(12)).toEqual("12s");
+  });
+
+  it(`m s`, () => {
+    expect(formatTimedelta(62)).toEqual("1m 2s");
+  });
+
+  it(`m ss`, () => {
+    expect(formatTimedelta(72)).toEqual("1m 12s");
+  });
+
+  it(`mm s`, () => {
+    expect(formatTimedelta(602)).toEqual("10m 2s");
+  });
+
+  it(`mm ss`, () => {
+    expect(formatTimedelta(612)).toEqual("10m 12s");
+  });
+
+  it(`h mm ss`, () => {
+    expect(formatTimedelta(4212)).toEqual("1h 10m 12s");
+  });
+
+  it(`hh mm ss`, () => {
+    expect(formatTimedelta(7812)).toEqual("2h 10m 12s");
+  });
+
+  it(`day h m ss`, () => {
+    const oneDay = 1 * (60 * 60 * 24);
+    const seconds = 12;
+    const delta = oneDay + seconds;
+    expect(formatTimedelta(delta)).toEqual("1 day 0h 0m 12s");
+  });
+
+  it(`days h m ss`, () => {
+    const oneDay = 2 * (60 * 60 * 24);
+    const seconds = 12;
+    const delta = oneDay + seconds;
+    expect(formatTimedelta(delta)).toEqual("2 days 0h 0m 12s");
+  });
+});
--- webapp/src/domain/datetimeUtils.ts
+++ webapp/src/domain/datetimeUtils.ts
@@ -1,3 +1,11 @@
+import { divmod } from "./math";
+
+const SECONDS_PER_DAY = 60 * 60 * 24;
+const SECONDS_PER_HOUR = 60 * 60;
+const SECONDS_PER_MINUTE = 60;
+
+export type Seconds = number; // duration in seconds - generic, nothing to do with epoch
+
 export function now(): Date {
   return new Date(new Date().setMilliseconds(0));
 }
@@ -19,3 +27,31 @@ export const isoDateFormatter = new Intl.DateTimeFormat(LANGUAGE_SIMILAR_TO_ISO8
   month: "2-digit",
   day: "2-digit",
 }).format;
+
+export function formatTimedelta(delta: Seconds): string {
+  let remainder = delta;
+
+  const chunks: string[] = [];
+
+  let [days, h, m, s] = [0, 0, 0, 0];
+
+  [days, remainder] = divmod(remainder, SECONDS_PER_DAY);
+  if (days > 0) {
+    chunks.push(`${days} ${days === 1 ? "day" : "days"}`);
+  }
+
+  [h, remainder] = divmod(remainder, SECONDS_PER_HOUR);
+  if (h > 0 || chunks.length > 0) {
+    chunks.push(`${h}h`);
+  }
+
+  [m, remainder] = divmod(remainder, SECONDS_PER_MINUTE);
+  if (m > 0 || chunks.length > 0) {
+    chunks.push(`${m}m`);
+  }
+
+  [s, remainder] = divmod(remainder, 1); // drop ms
+  chunks.push(`${s}s`);
+
+  return chunks.join(" ");
+}
