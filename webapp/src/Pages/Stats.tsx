ffb6a6b5d371e297a7d4a83405204bad8898401e
David Torralba Goitia <david.torralba.goitia@gmail.com>
2023-04-14 09:03:53 +0100

    feat: add stats page

---
+++ webapp/src/Pages/Stats.tsx
@@ -0,0 +1,99 @@
+import CenteredPage from "../components/CenteredPage";
+import NavBar from "../components/NavBar";
+import { ActivityManager } from "../domain/activities";
+import {
+  CompletedActivityManager,
+  getLastOccurrences,
+} from "../domain/completedActivities";
+import { Seconds, formatTimedelta, isoDateFormatter, now } from "../domain/datetimeUtils";
+import { ActivityId, ActivityName, CompletedActivity } from "../domain/model";
+import BlueprintThemeProvider from "../style/theme";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+
+interface HydratedCompletedActivity extends CompletedActivity {
+  activityName: ActivityName;
+  sinceCompleted: Seconds;
+}
+
+interface Props {
+  activityManager: ActivityManager;
+  completedActivityManager: CompletedActivityManager;
+}
+
+function StatsPage({ activityManager, completedActivityManager }: Props) {
+  const [history, setHistory] = useState<CompletedActivity[]>([]);
+
+  const _now = now();
+
+  const computeTimedelta = (completed: CompletedActivity): Seconds =>
+    (_now.getTime() - completed.date.getTime()) / 1000;
+
+  useEffect(() => {
+    completedActivityManager.changes$.subscribe((_) => {
+      const history = completedActivityManager.getAll();
+      setHistory(history);
+    });
+
+    const history = completedActivityManager.getAll();
+    setHistory(history);
+  }, [activityManager, completedActivityManager]);
+
+  function getActivityName(id: ActivityId): ActivityName {
+    return activityManager.get(id)?.name || `Activity ${id} not found`;
+  }
+
+  function hydrate(completed: CompletedActivity): HydratedCompletedActivity {
+    return {
+      ...completed,
+      sinceCompleted: computeTimedelta(completed),
+      activityName: getActivityName(completed.activityId),
+    };
+  }
+
+  const lastOcurrences = getLastOccurrences(history).map(hydrate);
+
+  return (
+    <BlueprintThemeProvider>
+      <CenteredPage>
+        <NavBar />
+        <h3>Last completed activities</h3>
+        <div>
+          {lastOcurrences.length > 0 ? (
+            <ul>
+              {lastOcurrences.map((completedActivity, i) => (
+                <LastOcurrence completed={completedActivity} key={i} />
+              ))}
+            </ul>
+          ) : null}
+        </div>
+      </CenteredPage>
+    </BlueprintThemeProvider>
+  );
+}
+
+export default StatsPage;
+
+const OrderedListContainer = styled.ol`
+  display: flex;
+  column-gap: 1rem;
+  justify-content: space-between;
+
+  margin-bottom: 0.2rem;
+`;
+
+const SinceCompleted = styled.div``;
+const Name = styled.div``;
+const CompletionDate = styled.div``;
+
+function LastOcurrence({ completed }: { completed: HydratedCompletedActivity }) {
+  const delta = formatTimedelta(completed.sinceCompleted);
+  const days = delta.split(" ").slice(0, 2).join(" ");
+  return (
+    <OrderedListContainer>
+      <SinceCompleted>{days}</SinceCompleted>
+      <Name>{completed.activityName}</Name>
+      <CompletionDate>{isoDateFormatter(completed.date)}</CompletionDate>
+    </OrderedListContainer>
+  );
+}
