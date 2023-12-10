310fec8aa68a506052164ac058f70641f9cbfd84
David Torralba Goitia <david.torralba.goitia@gmail.com>
2023-12-10 16:47:19 +0000

    feat(ui): support more set operations

--- webapp/src/lib/setOperations.ts
+++ webapp/src/lib/setOperations.ts
@@ -12,3 +12,43 @@ export function setsAreEqual<T>(a: Set<T> | undefined, b: Set<T> | undefined): b
 
   return true;
 }
+
+/**
+ * Add item to a set without mutating the original set
+ */
+export function addToSet<T>(set: Set<T>, item: T): Set<T> {
+  return new Set<T>([...set, item]);
+}
+
+/**
+ * Delete item from a set without mutating the original set
+ */
+export function deleteFromSet<T>(set: Set<T>, item: T): Set<T> {
+  const result = new Set<T>([...set]);
+  result.delete(item);
+  return result;
+}
+
+/**
+ * Identify which items are in `a` but not in `b` and in `b` but not in `a`.
+ * @param a set A
+ * @param b set B
+ */
+export function assessSetOverlap<T>({ a, b }: { a: Set<T>; b: Set<T> }): {
+  inAButNotInB: Set<T>;
+  inBButNotInA: Set<T>;
+} {
+  const in_a_not_b = new Set<T>();
+  const in_b_not_a = new Set([...b]);
+
+  for (const element_a of a) {
+    if (b.has(element_a)) {
+      in_b_not_a.delete(element_a);
+    } else {
+      in_a_not_b.add(element_a);
+    }
+  }
+
+  // return in_a_not_b;
+  return { inAButNotInB: in_a_not_b, inBButNotInA: in_b_not_a };
+}
