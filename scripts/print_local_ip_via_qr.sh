a11b9af5e88f4db6a2fd0ba233d0ace6b03b3600
David Torralba Goitia <david.torralba.goitia@gmail.com>
2024-01-14 20:23:56 +0000

    devex(qr): replace container with binary

--- scripts/print_local_ip_via_qr.sh
+++ scripts/print_local_ip_via_qr.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env bash
 
-local_ip=$(scripts/get_local_ip.py)
-echo "Local IP: ${local_ip}"
+set -o errexit
+set -o pipefail
+set -o nounset
 
-docker-compose run --rm qr python generate.py "${local_ip}"
+log () {
+    message="${1}"
+    echo >&2 "${message}"
+}
+
+local_ip=$(scripts/get_local_ip.py)
+log "Local IP: ${local_ip}"
+
+url="http://${local_ip}:3000/shopping-list"
+
+log "Scan the QR code bellow to access the app:"
+log ""
+scripts/qr "${url}" 2>/dev/null
+log ""
+log "${url}"
+log ""
